0243ec24dd288e21aac0a54b0f2ea5e4759011b7
codeoneline <mutouji1@163.com>
2020-05-05 08:37:46 +0800

    add travis ci

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
-'use strict';
+// regex that checks if value contains digits only
+const digitsOnly = /^\d+$/;
 
-function sayHiTo(name) {
-  return `Hi, ${name}`;
+// function to mask digits into US phone format
+function maskUSPhone(phone) {
+  // returning null when getting null, undefined, or an object that is no string as a parameter
+  if (!phone || typeof phone !== 'string') {
+    return null;
+  }
+
+  // returning the untouched value when it contains non-digit chars or when it has a length != 10
+  if (digitsOnly.test(phone) === false || phone.length !== 10) {
+    return phone;
+  }
+
+  // returning the masked value
+  const codeArea = phone.substring(0, 3);
+  const prefix = phone.substring(3, 6);
+  const suffix = phone.substring(6, 10);
+  return `(${codeArea}) ${prefix}-${suffix}`;
 }
 
-const message = sayHiTo('Bruno');
-
-console.log(message);
-
+export default maskUSPhone;
