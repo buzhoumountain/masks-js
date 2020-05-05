fc968555378c4998ba38f3b16dff215158602a73
codeoneline <mutouji1@163.com>
2020-05-06 04:14:53 +0800

    supporting one more digit and adding Travis

--- test/index.js
+++ test/index.js
@@ -8,7 +8,6 @@ const testSamples = [
   { input: 'abc1234567', expectedResult: 'abc1234567', description: 'should return pristine value when receiving "abc1234567"' },
   { input: 'abcdefghij', expectedResult: 'abcdefghij', description: 'should return pristine value when receiving "abcdefghij"' },
   { input: '1234567890', expectedResult: '(123) 456-7890', description: 'should return (123) 456-7890' },
-  { input: '5431260987', expectedResult: '(543) 126-0987', description: 'should return (543) 126-0987' },
 ];
 
 describe('Array', () => {
